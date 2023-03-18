706e6655f9ab4842b61cf790dfe4695279b73102
PAVLO DEMENSHYN <89635409+Paul-Dimenshion@users.noreply.github.com>
2023-03-18 14:14:39 -0300

    comments for routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
-const userRoutes = require('./user-routes');
-const thoughtRoutes = require('./thought-routes');
+const router = require('express').Router(); // creates a new instance of the Express.js Router middleware.
+const userRoutes = require('./user-routes'); // imports the user routes from the user-routes.js file.
+const thoughtRoutes = require('./thought-routes'); // imports the thought routes from the thought-routes.js file.
 
-router.use('/users', userRoutes);
-router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes); // sets up a prefix of /users for all of the user routes defined in the user-routes.js file.
+router.use('/thoughts', thoughtRoutes); // sets up a prefix of /thoughts for all of the thought routes defined in the thought-routes.js file.
 
-module.exports = router;
+module.exports = router; // exports the router middleware for use in the main server file.
