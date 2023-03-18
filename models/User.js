1511ab33896f252ab3440b34de72e0187f471c79
PAVLO DEMENSHYN <89635409+Paul-Dimenshion@users.noreply.github.com>
2023-03-18 14:52:12 -0300

    comments for models

--- models/User.js
+++ models/User.js
@@ -1,6 +1,13 @@
+/* This line imports the Schema and model objects from the mongoose package, and the moment package. 
+Schema is used to define the structure of documents stored in the database, 
+and model is used to create and manage database collections based on a schema.*/
 const { Schema, model } = require('mongoose');
 const moment = require('moment');
 
+/* This block of code defines a Mongoose schema called UserSchema, which specifies the structure of documents in the users collection. 
+The schema has four fields: username, email, thoughts, and friends. The username and email fields are required and must be unique, 
+while thoughts and friends are arrays of references to other documents in the database. 
+The toJSON option is set to include virtual properties when the document is converted to JSON.*/
 const UserSchema = new Schema({
     username: {
         type: String,
@@ -35,12 +42,16 @@ const UserSchema = new Schema({
     }
 );
 
+/* This line defines a virtual property called friendCount on the UserSchema. 
+The get function returns the length of the friends array, which represents the number of friends a user has.*/
 UserSchema.virtual('friendCount').get(function () {
     return this.friends.length;
 });
 
-// create the User model using the UserSchema
+/* This line creates a Mongoose model called User using the UserSchema that was defined earlier. 
+The model represents a collection of documents in the database. */
 const User = model('User', UserSchema);
 
-// export the User model
+/* This line exports the User model so that it can be used in other parts of the application. 
+The User model can be required in other files using the require function. */
 module.exports = User;
--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const User = require('./User');
-const Thought = require('./Thought');
+const User = require('./User'); // imports the User model from the ./User module.
+const Thought = require('./Thought'); // imports the Thought model from the ./Thought module.
 
-module.exports = { User, Thought };
+module.exports = { User, Thought }; //  exports an object containing the User and Thought models for use in other modules.
